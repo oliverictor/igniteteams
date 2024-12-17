af382fa09abcb01d0a548dd828a86cc0f9e70789
victor.oliveira <victor.oliveira@hexagon.pro>
2024-12-16 21:04:18 -0300

    add loading

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,16 @@
 
+import { Loading } from '@components/Loading';
+import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
-import { ThemeProvider } from 'styled-components';
 import theme from 'src/theme';
+import { ThemeProvider } from 'styled-components';
 
 export default function HomeScreen() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
 }
