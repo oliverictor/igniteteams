2bdc31c555443569d46e4dc7533108ee99e3cdaa
victor.oliveira <victor.oliveira@hexagon.pro>
2024-12-19 08:58:08 -0300

    add component Header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,6 +2,7 @@
 import { Loading } from '@components/Loading';
 import { Roboto_400Regular, Roboto_700Bold, useFonts } from '@expo-google-fonts/roboto';
 import { Groups } from '@screens/Groups';
+import { StatusBar } from 'react-native';
 import theme from 'src/theme';
 import { ThemeProvider } from 'styled-components';
 
@@ -10,6 +11,11 @@ export default function HomeScreen() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
